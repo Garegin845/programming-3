b317d331f370fd93e858a57c230f5a8c1666d6c3
BigBaldHead26 <karapet.m2@tumo.org>
2023-11-01 18:44:27 +0400

    First commit. ok good

---
+++ GameOfLifeGaregin14/Garegin/bomb.js
@@ -0,0 +1,20 @@
+class Bomb {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.directions = [];
+  }
+
+  getNewCoordinates() {
+    this.directions = [
+      [this.x - 1, this.y - 1],
+      [this.x, this.y - 1],
+      [this.x + 1, this.y - 1],
+      [this.x - 1, this.y],
+      [this.x + 1, this.y],
+      [this.x - 1, this.y + 1],
+      [this.x, this.y + 1],
+      [this.x + 1, this.y + 1],
+    ];
+  }
+}
