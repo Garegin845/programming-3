525cecb33e6e07be901602c688fa98746fe3f822
ARMGor10 <gor.gevorgyan8@tumo.org>
2023-11-04 13:25:31 +0400

    ADD NEW CLAS

--- GameOfLifeGaregin14/Garegin/grassEater.js
+++ GameOfLifeGaregin14/Garegin/grassEater.js
@@ -1,9 +1,8 @@
-class GrassEater {
+class GrassEater extends LivingCreature {
   constructor(x, y) {
-    this.x = x;
-    this.y = y;
-    this.energy = 10;
-    this.directions = [];
+       super(x,y)
+      this.energy = 10;
+ 
   }
 
   getNewCoordinates() {
@@ -19,26 +18,9 @@ class GrassEater {
     ];
   }
 
-  chooseCell(char, char1) {
+  chooseCell(char) {
     this.getNewCoordinates();
-    let found = [];
-
-    for (let i in this.directions) {
-      let x = this.directions[i][0];
-      let y = this.directions[i][1];
-      if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
-        if (matrix[y][x] == char) {
-          found.push(this.directions[i]);
-        }
-      }
-      if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
-        if (matrix[y][x] == char1) {
-          found.push(this.directions[i]);
-        }
-      }
-    }
-
-    return found;
+    return super.chooseCell(char)
   }
 
   mul() {
